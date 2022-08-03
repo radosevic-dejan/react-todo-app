d28ffd1d7451514397ac7a2131dbedd2fc28c510
radosevic-dejan <radosevic.dejan@proton.me>
2022-08-03 15:58:41 +0200

    Adding and removing items done, rest in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,79 @@
 import { useState, useEffect } from "react";
 import { MinusCircleIcon } from "@heroicons/react/solid";
 
-const getData = () => {
-  const todos = localStorage.getItem("todos");
-  return todos ? JSON.parse(todos) : [];
-}
+const getLocalData = () => {
+  const data = localStorage.getItem("todos");
+  if (data) return JSON.parse(data);
+  return [];
+};
 
 function App() {
-  const [todos, setTodos] = useState(getData());
+  const [todos, setTodos] = useState(getLocalData());
   const [input, setInput] = useState("");
 
-  const addTodo = () => {
-    localStorage.setItem("todos", JSON.stringify(input));
-    setInput("");
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const timeCreated = new Date().getTime();
 
-  const handleInput = e => {
+    let todoObject = {
+      id: timeCreated,
+      task: input,
+      completed: false,
+    };
+
+    setTodos([...todos, todoObject]);
+    setInput("");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleChange = (e) => {
     setInput(e.target.value);
-    console.log(input)
+  };
+
+  const handleDelete = (id) => {
+    const filtered = todos.filter((todo) => todo.id !== id);
+    setTodos(filtered);
   };
 
   return (
     <div className="container max-w-sm mx-auto mt-10 h-[80vh] rounded-xl border border-slate-700 flex flex-col">
       <h1 className="text-center p-2 font-bold my-4">To Do App</h1>
 
-      <form className="p-4">
+      <form className="p-4" onSubmit={handleSubmit}>
         <input
           className="border border-stone-700 rounded-xl py-2 px-1"
           type="text"
           placeholder="Add new task"
           value={input}
-          onChange={(e) => handleInput(e)}
+          onChange={(e) => handleChange(e)}
         />
         <button
           type="submit"
           className="border border-stone-700 rounded-xl px-4 py-2 ml-2"
-          onClick={addTodo}
         >
           Add
         </button>
       </form>
 
-      <div className="my-2">{
-         
-      }</div>
+      <div className="my-2">
+        {todos.length === 0 ? (
+          <p className="p-4">No data to display</p>
+        ) : (
+          todos.map((todo) => {
+            return (
+              <div key={todo.id} className="flex justify-between px-4">
+                <p>{todo.task}</p>{" "}
+                <button onClick={() => handleDelete(todo.id)}>
+                  <MinusCircleIcon className="w-6 h-6" />
+                </button>
+              </div>
+            );
+          })
+        )}
+      </div>
     </div>
   );
 }
